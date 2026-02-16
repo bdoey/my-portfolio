8ea9b28e32c8e682fa720fe24be6348104f6093b
Brandon Doey <mr.bdoey@gmail.com>
2026-02-16 14:15:07 -0500

    Update image source in About component

--- components/About.tsx
+++ components/About.tsx
@@ -44,11 +44,11 @@ const About: React.FC = () => {
                         
                         {/* Image Placeholder - User should replace src with actual image */}
                         <img 
-                            src="/headshot2.png" 
+                            src="/headshot.jpg" 
                             alt={PERSONAL_INFO.name}
                             className="w-full h-full object-cover opacity-90 hover:opacity-100 transition-opacity duration-500"
                             onError={(e) => {
-                                e.currentTarget.src = "/headshot2.png";
+                                e.currentTarget.src = "/headshot.jpg";
                             }}
                         />
                         
